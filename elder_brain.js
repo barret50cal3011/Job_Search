33627900dabe30a2d3ef4fee8674e179dcabbc43
B4RRET <barret50cal3011@gmail.com>
2022-11-16 15:54:57 -0600

    added color to the page, added an option to get all the jobs and added coments to the backend code

--- elder_brain.js
+++ elder_brain.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+//list with all the job postulations
 let jobs = [];
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.static("./front"));
 app.use(cors());
 app.use(express.json());
 
+//compares 2 jobs to see if they are equal
 function comp_job(job1 ,job2){
     return (
         job1.source === job2.source &&
@@ -19,6 +21,7 @@ function comp_job(job1 ,job2){
     );
 }
 
+// gets the job that will be added to the list and adds it
 app.post("/add/:job", (req, res) =>{
     let new_job = req.body.job;
     let is_added = false;
@@ -33,13 +36,13 @@ app.post("/add/:job", (req, res) =>{
     res.end();
 });
 
+//search the postulations on the list
 app.post("/search/:company", (req, res) => {
-    let company = req.body.company;
-    console.log("Looking for:" + company);
+    let company = req.body.company || "";
 
     let searched_jobs = [];
     for(let i = 0; i < jobs.length; i++){
-        if(jobs[i].company === company){
+        if(jobs[i].company === company || company === ""){
             searched_jobs.push(jobs[i]);
         }
     }
@@ -52,6 +55,7 @@ app.post("/search/:company", (req, res) => {
     res.send({searched_jobs});
 });
 
+//gets a job and the new status for the job and changes the status of the given job in the list
 app.post("/edit/:job_to_edit", (req, res) => {
     let job_to_edit = req.body.job_to_edit;
     let new_status = req.body.new_status;
@@ -65,6 +69,22 @@ app.post("/edit/:job_to_edit", (req, res) => {
     res.end();
 });
 
+//gets all the job postulations in the list
+app.get("/get-all/", (req, res) => {
+    all_jobs = [];
+    jobs.forEach((job) => {
+        all_jobs.push(job);
+    });
+
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+    
+    res.send({all_jobs});
+});
+
+//start the server
 app.listen(8080, () => {
     console.log("Listening");
 });
